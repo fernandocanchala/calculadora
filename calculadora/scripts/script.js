9da8f90af3238002e848b36904759408156e3f4b
fernando canchala <william0893@hotmail.com>
2015-02-19 11:33:33 -0500

    primer commit calcu

---
+++ calculadora/scripts/script.js
@@ -0,0 +1,11 @@
+function calcula()
+	  {
+	     /*var numero;
+		 numero=document.form1.txt.value;
+		 document.form1.txt_rta.value=numero.length;*/
+		 var operando1 = document.calc.operando1.value
+    var operando2 = document.calc.operando2.value 
+    var result = eval(operando1 + operacion + operando2)
+    document.calc.resultado.value = result
+		 
+	  }
